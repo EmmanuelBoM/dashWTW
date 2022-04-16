cfdf29d828c265c913e613f7ab07d66d65ec1cbb
Diana Aguirre <a01276380@itesm.mx>
2022-04-16 02:59:55 -0500

    Añade cards de gráfica de pie, botón de regreso y barrita de progreso

--- src/pages/MapDetails/MapDetails.page.tsx
+++ src/pages/MapDetails/MapDetails.page.tsx
@@ -10,20 +10,34 @@ import {
   Box,
   Text,
   Image,
-  Divider
+  Divider,
+  CircularProgress, 
+  CircularProgressLabel,
+  Switch,
+  Button
 } from "@chakra-ui/react"
 
-import MapsTable from "../../components/MapsTable";
+// import MapsTable from "../../components/MapsTable";
 import MenuComponent from "../../components/Menu/menu.component";
 import hotelLogo from "../../assets/hotelLogo.png"
 import locationIcon from "../../assets/locationIcon.png"
 import MapProgressbar from "../../components/MapProgressbar";
+
+import { RiArrowGoBackLine } from "react-icons/ri";
 
 let hotelName:string = "The Grand Mayan"
 let category:string = "Place To Stay"
 let mapperName:string = "Tom Cruise"
 let acommodationType:string = "Hotel"
 let hotelAddress:string = "Blvrd Riviera Nayarit 254, 63735 Nuevo Vallarta, Nay."
+
+// These vars contain the mapped areas and their progress, respectively
+let areasMapped:string[] = ["Building Entrance", "Food Service Area", "Lobby/Reception"]
+let percentagePerMappedArea:number[] = [100, 70, 50]
+
+// These vars contain the statuses of completion of all maps
+let progressCompletionMaps:string[] = ["In progress", "Completed", "Not started"]
+let colorsProgressCompletionMaps:string[] = ["#FFB800", "green", "red"]
 
 export const MapDetails = () => (
 	<Container maxWidth="container.xxl" bg='#f8f9d'>
@@ -33,15 +47,30 @@ export const MapDetails = () => (
 				<HStack w='70vw' justifyContent='space-between'>
 					<VStack alignItems='flex-start'>
 						<HStack>
-							<Heading size="xl">{hotelName}</Heading>
-							{/*TO DO: Barrita de progreso*/}
+							<Heading size="xl" marginRight='1.5vw'>{hotelName}</Heading>
+              <Box backgroundColor={colorsProgressCompletionMaps[0]}
+                   p='0% 2% 0% 2%'
+                   w='7vw'
+                   textAlign='center'
+                   borderRadius='2em'
+                   borderWidth='3px'
+                   borderColor='#858585'
+                   color='black.700'
+                   fontWeight='bold'
+                   >{progressCompletionMaps[0]}</Box>
 						</HStack>
 						
 						<Text color='black.400' fontSize="xl">Accesibility Mapping Progress | <b>{category}</b> </Text>
 					</VStack>
-
-					{/*TO DO: Botón de regreso */}
-				</HStack>
+          <Button rightIcon={<RiArrowGoBackLine/>}
+                  background='transparent'
+                  fontSize='3em'
+                  width='1.5em'
+                  height='1.5em'
+                  borderRadius='2em'
+                  _hover={{ bg: 'white' }}>
+          </Button>				
+        </HStack>
 
 				
 				<HStack w="full" spacing={4} justifyContent="space-between" alignItems="top" >
@@ -109,6 +138,40 @@ export const MapDetails = () => (
 						</VStack>
 					</HStack>
 				</VStack>
+
+				<HStack w='full'>
+          <Grid column={3} columnGap={3} rowGap={3} w="full" h='3vw' marginTop='3vw' color='black.main'>
+            <GridItem colSpan={2} bg='blue.main' h='4px' marginTop='0.7vw'>
+              <HStack width='full' height='0.9em' borderWidth='1px' borderColor='transparent' borderBottomColor='white'></HStack>
+            </GridItem>
+            <GridItem colSpan={1} textAlign='center' paddingTop={0}> <Heading fontSize='xl'>Areas Progress</Heading></GridItem>
+            <GridItem colStart={4} colEnd={6} bg='blue.main' h='4px' marginTop='0.7vw'>
+              <HStack width='full' height='0.9em' borderWidth='1px' borderColor='transparent' borderBottomColor='white'></HStack>
+            </GridItem>
+          </Grid>
+        </HStack>
+        
+        <Grid templateColumns='repeat(3, 1fr)'
+              gap={4}
+              w="full">
+            {areasMapped.map((area:string, index:number) => (
+                <GridItem>
+                  <VStack p={5} shadow='md' borderWidth='1px'  w='22vw' borderRadius='lg'>
+                    <Heading fontSize='xl' color='black.main' fontWeight='bold' marginTop='1vw' marginBottom='2vw'>{area}</Heading>
+                    <CircularProgress value={percentagePerMappedArea[index]} size='120px'>
+                      <CircularProgressLabel color='#2F6FE4' fontSize='lg' fontWeight='bolder'>{percentagePerMappedArea[index]}%</CircularProgressLabel>
+                    </CircularProgress>
+                  </VStack>
+                </GridItem>
+              )
+            )}
+        </Grid>
+
+        <HStack alignItems='left' width='100%'>
+          <Text textAlign='left' color='black.500' size='lg' fontWeight='bold'>Show non completed areas</Text>
+          <Switch size='lg'/>
+        </HStack>
+
 			</VStack>
 		</Flex>	
 	</Container>
