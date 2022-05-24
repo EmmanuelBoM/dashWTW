9e2c807875a8bba88a37d8c09a3a0a023e2cc368
EmmanuelBoM <a01276310@itesm.mx>
2022-05-23 21:27:39 -0500

    Table in Maps Overview page receives dates and works

---
+++ src/components/MapsTablePicker/mapsTablePicker.types.tsx
@@ -0,0 +1,12 @@
+export interface IPropTypes {
+    calendarStartDate: any
+    calendarEndDate: any
+}
+
+export interface IData {
+    placeID: number,
+    placeName: string, 
+    city: string, 
+    progress: number,
+    isNumeric: boolean
+};
